7a5761e0a705d8d3fc0ad8259950327a246086e2
Abu Saleem <abusaleem>
2018-11-06 22:29:59 +0100

    The certificate module was created

---
+++ Modules/Certificate/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('certificate::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('certificate.name') !!}
+    </p>
+@stop
