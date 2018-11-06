d5a2fd5beeff889580046667093d888483dd0ae4
Abu Saleem <abusaleem>
2018-11-06 22:25:52 +0100

    The search module was created

---
+++ Modules/Search/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('search::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('search.name') !!}
+    </p>
+@stop
